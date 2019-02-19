4493ab75b2b5ca4889b79f99650e12b092c10c69
missisol <missisololol@gmail.com>
2019-02-19 13:57:50 +0300

    Class MyUnitTest created in frontend

---
+++ frontend/tests/unit/MyUnitTest.php
@@ -0,0 +1,60 @@
+<?php namespace frontend\tests;
+
+use frontend\models\ContactForm;
+
+class MyUnitTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \frontend\tests\UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+      $var = 6;
+      $arr = [
+        'one' => 1,
+        'two' => 5,
+        'three' => 7,
+      ];
+
+      $name = [
+        0 => 'Vasya',
+        1 => 'xxx',
+        2 => 'yyy',
+      ];
+      $email = 'vvv@vvv';
+      $model = new ContactForm();
+      $model->setAttributes([
+        'name' => $name,
+        'email' => $email,
+      ]);
+
+      $username = [
+        0 => 'Vasya',
+        1 => 'xxx',
+        2 => 'yyy',
+      ];
+
+      $this->assertTrue(!empty($var), 'Comparison with true, checking whether a variable is empty');
+      expect(is_array($arr))->true();
+
+      $this->assertEquals($var, 6, 'assertEquals');
+
+      $this->assertLessThan($var, 2, 'assertLessThen');
+
+      $this->assertAttributeEquals($username, 'name', $model);
+      expect($email, $model->email)->equals('vvv@vvv');
+
+      $this->assertArrayHasKey(1, $username, 'Checking whether a key is an array');
+    }
+}
