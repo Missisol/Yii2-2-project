452d13ebb3e7484b1167a5efc6585ef52dc1888f
missisol <missisololol@gmail.com>
2019-02-23 18:59:48 +0300

    Chat created

---
+++ common/modules/chat/widgets/views/chat.php
@@ -0,0 +1,9 @@
+<div id="chatForm">
+  <label for="bodyMessage">Enter your message</label>
+  <textarea id="bodyMessage" cols="60" rows="2" placeholder="message text"></textarea>
+  <button id="chatSubmit">Submit</button>
+</div>
+<h4>All messages</h4>
+<ul id="chatMessages"></ul>
+
+
