6b74c30fe5fc518007adf15ec907d0ce043f7ada
missisol <missisololol@gmail.com>
2019-03-10 17:38:17 +0300

    The pseudo class Application created

---
+++ ide/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace yii\web;
+
+use common\services\EmailService;
+use common\services\ProjectService;
+use common\services\NotificationService;
+
+/**
+ * Class Application
+ * @package yii\web\Application
+ * @property ProjectService $projectService
+ * @property EmailService $emailService
+ * @property NotificationService $notificationService
+ */
+class Application
+{
+
+}
