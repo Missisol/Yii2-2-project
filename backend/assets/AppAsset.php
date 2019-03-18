a0954e21ae96ed0c9da9ca8f5b9b2302a0e106bf
missisol <missisololol@gmail.com>
2019-03-18 17:15:18 +0300

    Assets setup updated

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -15,6 +15,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        '\y2aa-frontend.test\js/chat.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
