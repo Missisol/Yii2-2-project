91c9ffd09188aec684b9df4380c7e3765aba510c
missisol <missisololol@gmail.com>
2019-03-19 11:18:57 +0300

    Methods added

--- common/models/query/ProjectUserQuery.php
+++ common/models/query/ProjectUserQuery.php
@@ -42,6 +42,26 @@ class ProjectUserQuery extends \yii\db\ActiveQuery
     }
 
     /**
+     * @param $projectId
+     * @return ProjectUserQuery
+     */
+    public function byProjectManager($projectId)
+    {
+        return $this->andWhere(['role' => ProjectUser::ROLE_MANAGER])
+            ->andWhere(['project_id' => $projectId]);
+    }
+
+    /**
+     * @param $projectId
+     * @return ProjectUserQuery
+     */
+    public function byProjectTester($projectId)
+    {
+        return $this->andWhere(['role' => ProjectUser::ROLE_TESTER])
+            ->andWhere(['project_id' => $projectId]);
+    }
+
+    /**
      * {@inheritdoc}
      * @return \common\models\ProjectUser[]|array
      */
